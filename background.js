4263742589b7244b4968ec87d173fe0ea3f73b2b
forgo <127855426+forgooo@users.noreply.github.com>
2024-02-20 15:09:02 +0300

    Delete background.js

--- background.js
+++
@@ -1,7 +0,0 @@
-// Background script (background.js)
-// This script runs in the background and can be used to handle events or perform tasks that need to happen independently of the web pages.
-
-// Example: Listen for messages from content script and perform actions
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    // Handle messages here
-});
